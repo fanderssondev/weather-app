1baa5d1da50039fffb477f3f5fb7638b5bfe6900
nanobyter <sotarfredrik@gmail.com>
2023-06-03 18:53:47 +0200

    Ran build on site

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import { getWeather } from './weather';
 import { ICON_MAP } from './iconMap';
 
+// https://open-meteo.com/en/docs#latitude=41.41&longitude=2.15&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&windspeed_unit=ms&timeformat=unixtime&timezone=Europe%2FBerlin
+
 navigator.geolocation.getCurrentPosition(positionSuccess, positionError);
 
 function positionSuccess({ coords }) {
